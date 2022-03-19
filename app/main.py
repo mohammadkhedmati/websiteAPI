d166218c43ea5b6da60fa0e4725293a9b84fb82b
mohammadkhedmati <mohammad.khedmati2012@gmail.com>
2022-03-19 08:20:06 +0330

    add docs

--- app/main.py
+++ app/main.py
@@ -7,7 +7,25 @@ class allinfo(BaseModel):
     url: str
 
 
-app = FastAPI()
+description = """
+Instagram API helps you to get website SEO details. 🚀
+"""
+app = FastAPI(
+    title="Website API",
+    description=description,
+    version="0.0.1",
+    terms_of_service="https://faraabin.com/",
+    contact={
+        "name": "mohammad khedmati",
+        "url": "https://mkhedmati.ir",
+        "email": "mohammad.khedmati2012@gmail.com",
+    },
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+)
+
 
 
 @app.get("/")
